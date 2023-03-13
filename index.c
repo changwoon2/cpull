92c1c1f3f9c9e732867baf473b5e7d04d7b7f403
BeYoddy <changwoon4@naver.com>
2023-03-13 16:40:08 +0900

    첫수업

--- index.c
+++ index.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
-int main() {
-  printf("Hello World!");
+int main()
+{
+  // printf("Hello World!\n");
+  // printf("Hello World!\n");
+  // int age = 20;
+  // printf("%d\n",age);
+  // age = 21;
+  // printf("%d\n",age);
+  // float f =46.5f;
+  // printf("%f\n",f);
+  // int v1, v2, sum;
+  // v1 = 987;
+  // v2 = 4;
+  // sum = v1 + v2;
+
+  int v1 = 987;
+  int v2 = 4;
+  int sum = v1 + v2;
+  printf("%d + %d = %d\n", v1, v2, sum);
+
+  int dex = v1 - v2;
+  printf("%d - %d = %d\n", v1, v2, dex);
+
+  int def = v1 * v2;
+  printf("%d * %d = %d\n", v1, v2, def);
+
+  int str = v1 / v2;
+  printf("%d / %d = %d\n", v1, v2, str);
+
   return 0;
 }
